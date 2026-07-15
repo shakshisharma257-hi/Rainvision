81da2e6d9753e3e3c8bcc1d9f784544236709597
shakshisharma257-hi <shakshisharma257@gmail.com>
2026-07-15 21:33:47 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
 from streamlit_option_menu import option_menu
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
